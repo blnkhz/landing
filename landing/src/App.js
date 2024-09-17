535da96667361aabcac1c27a3826cab35524aa47
Blanka Hooz <hooz.blanka@gmail.com>
2024-09-16 17:58:39 -0700

    add buttons for about page and message popup

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return '[feed me content pls]';
-}
+import { ContactForm } from "./components/contactForm";
+import "./App.css";
+import { useState } from "react";
+import { SplineCanvas } from "./components/scene";
+import { About } from "./components/about";
+
+const App = () => {
+  const [isContactFormVisible, setIsContactFormVisible] = useState(false);
+  const [isAboutVisible, setIsAboutVisible] = useState(false);
+  return (
+    <div className="main-content">
+      <button
+        onClick={() => setIsContactFormVisible(!isContactFormVisible)}
+        className="styled-button contact"
+        hidden={isContactFormVisible || isAboutVisible}
+      >
+        say hiiii
+      </button>
+      <button
+        onClick={() => setIsAboutVisible(!isAboutVisible)}
+        className="styled-button about"
+        hidden={isAboutVisible || isContactFormVisible}
+      >
+        about:blank<span className="tiny">a</span>
+      </button>
+      <SplineCanvas />
+      <ContactForm
+        isVisible={isContactFormVisible}
+        handleVisibility={setIsContactFormVisible}
+      />
+      <About isVisible={isAboutVisible} handleVisibility={setIsAboutVisible} />
+    </div>
+  );
+};
 
 export default App;
