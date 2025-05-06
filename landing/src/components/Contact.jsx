b3e1771d8713585111afef5bf7011005606aeb28
Blanka Hooz <hooz.blanka@gmail.com>
2025-05-06 14:34:05 -0700

    update button colors

--- landing/src/components/Contact.jsx
+++ landing/src/components/Contact.jsx
@@ -31,7 +31,7 @@ export default function ContactModal() {
     <>
       <button
         onClick={openModal}
-        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+        className="px-4 py-2 bg-rose-500 text-white rounded hover:bg-black"
       >
         say hi
       </button>
@@ -89,7 +89,7 @@ export default function ContactModal() {
                 </button>
                 <button
                   type="submit"
-                  className="px-4 py-2 bg-rose-500 text-white rounded hover:bg-blue-700"
+                  className="px-4 py-2 bg-rose-500 text-white rounded hover:bg-black"
                 >
                   submit
                 </button>
