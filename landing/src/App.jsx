65d95ffaf73db4cd6979ba5e9ca49b400333a091
Blanka Hooz <hooz.blanka@gmail.com>
2025-05-06 12:16:58 -0700

    add tailwind

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-  return <>[brb]</>
+  return (
+    <div className="p-4 bg-white text-gray-900 md:p-8 lg:p-12">
+      <h1 className="text-xl md:text-3xl font-bold">blnkhz</h1>
+      <p className="text-sm md:text-base">[tbd]</p>
+    </div>
+  )
 }
 
 export default App
